ec6b13abb3b39981ebb9160a4f1d5b36ad66eef8
haixiangyan <haixiang6123@gmail.com>
2025-04-24 23:30:36 +0800

    feat: 完成聊天室样式

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,14 +1,26 @@
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Avatar } from '@rneui/base';
-import { useLayoutEffect } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useLayoutEffect, useState } from 'react';
+import {
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ChatScreen = () => {
   const { params } = useRoute();
   const navigation = useNavigation();
 
   const { chatId, chatName } = params as any;
+
+  const [input, setInput] = useState('');
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -31,13 +43,47 @@ const ChatScreen = () => {
           <AntDesign name="arrowleft" size={24} color="white" />
         </TouchableOpacity>
       ),
+      headerRight: () => (
+        <View className="mr-5 w-20 flex-row justify-between">
+          <TouchableOpacity>
+            <FontAwesome name="video-camera" size={24} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name="call" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+      ),
     });
   }, []);
 
+  const sendMessage = () => {
+    // TODO: send message to firebase
+  };
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Chat Screen</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <StatusBar style="light" />
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className="w-full flex-1"
+        keyboardVerticalOffset={90}>
+        <>
+          <ScrollView />
+          <View className="w-full flex-row items-center p-4">
+            <TextInput
+              value={input}
+              onChangeText={setInput}
+              placeholder="Signal Message"
+              className="bottom-0 mr-4 h-12 flex-1 rounded-full border-none bg-[#ECECEC] p-3 color-gray-500"
+            />
+            <TouchableOpacity activeOpacity={0.5} onPress={sendMessage} className="">
+              <Ionicons name="send" size={24} color="#2B68E6" />
+            </TouchableOpacity>
+          </View>
+        </>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
